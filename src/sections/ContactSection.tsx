0f9d65f5833b8e4cb665045c41d0e52ccd4f3dbf
Anthony M. <59492035+Athom242@users.noreply.github.com>
2026-04-28 17:20:38 +0000

    Ajout des Composants de Section pour la page Home
    Structurations et definitions des composants tsx et de leurs fichiers
    scss respectifs

---
+++ src/sections/ContactSection.tsx
@@ -0,0 +1,13 @@
+import React,{useState} from "react";
+import styles from "../styles/sections/ContactSection.module.scss";
+
+
+const ContactSection:React.FC=()=>{
+    // const 
+    return(
+        <div className={styles.ContactSection}>
+
+        </div>
+    )
+}
+export default ContactSection;
