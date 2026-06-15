9fb8b0dd40778187e3879d79c4df730c9cde08ae
Anthony M. <59492035+Athom242@users.noreply.github.com>
2026-06-15 13:05:34 +0000

    modification elementatire mise en commentaire

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-=======
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
->>>>>>> 889e568 (Ajout de scss et de tailwind et structuration Style)
+// import App from './App.tsx'
+
 
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/routers.tsx";
