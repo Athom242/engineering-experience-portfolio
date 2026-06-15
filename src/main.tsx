cddc0a6f73df1e69d1286fdc4f0ed48419d234e4
Anthony M. <59492035+Athom242@users.noreply.github.com>
2026-06-15 16:49:28 +0000

    deplacement de fichier styles.modules.scss dans un dossier adapter

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import App from './App.tsx'
+import App from './App.tsx'
 
 
 import { RouterProvider } from "react-router-dom";
-import { router } from "./app/router.tsx";
+import { router } from "./routers.tsx";
 
 import "./styles/index.scss";
 
--- src/routers.tsx
+++ src/routers.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 
 import App from "../app/App";
 
-import Home from "../pages/Homes/Home";
+import Home from "../pages/Home/Home";
 import Projects from "../pages/Projects/Projects";
 import About from "../pages/About/About";
 import Engineering from "../pages/Engineering/Engineering";
