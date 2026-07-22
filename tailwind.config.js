5505165a3d7f5d58767ecc49acda424791acd088
Anthony M. <moussavouanthony@gmail.com>
2026-07-22 15:18:41 +0000

    mise a jour du fichier tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-<<<<<<< HEAD
-    "./src/**/*.{js,ts,jsx,tsx}",
-=======
     "./src/**/*.{js,ts,jsx,tsx,vue}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
->>>>>>> b88402c (Insertion du Footer de la page)
   ],
   theme: {
     extend: {},
